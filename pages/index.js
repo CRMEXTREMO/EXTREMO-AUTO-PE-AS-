a68e106fe57c511be44000313d726a1820730e8c
CRMEXTREMO <giovanniribeiromacedo@gmail.com>
2025-04-13 11:32:46 -0300

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: '2rem', textAlign: 'center' }}>
-      <h1>EXTREMO AUTO PEÇAS</h1>
-      <p>Seja bem-vindo à plataforma oficial!</p>
-    </div>
-  );
-}
