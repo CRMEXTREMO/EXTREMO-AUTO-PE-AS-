e2b39f1973c68ce08e4e3f0f62a4662c6aa1094c
CRMEXTREMO <giovanniribeiromacedo@gmail.com>
2025-04-13 14:02:39 -0300

    Delete pages/app/atendimentos/page.js

--- pages/app/atendimentos/page.js
+++
@@ -1,8 +0,0 @@
-export default function AtendimentosPage() {
-  return (
-    <div className="p-4">
-      <h1 className="text-xl font-bold">Atendimentos</h1>
-      <p>Aqui você verá todos os atendimentos realizados pela Autozone.</p>
-    </div>
-  );
-    }
