8cec9af3d7c66ef431656d2d3a4c1db261f47802
CRMEXTREMO <giovanniribeiromacedo@gmail.com>
2025-04-13 14:03:17 -0300

    Create index.js

---
+++ pages/pages/atendimentos/index.js
@@ -0,0 +1,12 @@
+import Header from '../../components/Header';
+
+export default function Atendimentos() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Atendimentos</h1>
+      </main>
+    </>
+  );
+    }
