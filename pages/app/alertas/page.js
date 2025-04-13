a4def6baf616e159886d0c5695bccfe38ab4cc94
CRMEXTREMO <giovanniribeiromacedo@gmail.com>
2025-04-13 10:35:30 -0300

    Create page.js

---
+++ pages/app/alertas/page.js
@@ -0,0 +1,8 @@
+export default function AlertasPage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold">Alertas</h1>
+      <p>Aqui serão exibidos os alertas automáticos do sistema.</p>
+    </div>
+  );
+}
