058f94ba1e42d62c3325bd7ad9bf697eac5601c4
CRMEXTREMO <giovanniribeiromacedo@gmail.com>
2025-04-13 13:20:49 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// pages/index.js
+export default function Home() {
+  return (
+    <div>
+      <h1>EXTREMO AUTO PEÇAS</h1>
+      <p>Seja bem-vindo à plataforma oficial!</p>
+    </div>
+  );
+    }
