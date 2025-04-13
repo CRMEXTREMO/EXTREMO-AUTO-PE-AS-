bffa0eaa41983521881cdb7da71b1eb1b44a3f7b
CRMEXTREMO <giovanniribeiromacedo@gmail.com>
2025-04-13 10:36:43 -0300

    Create page.js

---
+++ pages/app/app/orcamentos/page.js
@@ -0,0 +1,8 @@
+export default function OrcamentosPage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold">Orçamentos</h1>
+      <p>Visualize e gerencie os orçamentos da Autozone aqui.</p>
+    </div>
+  );
+    }
